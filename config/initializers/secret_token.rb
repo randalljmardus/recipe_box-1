68d417c2de1c2d96dc58d35df015d2be8d635954
zkirchin <zacharykirchin@gmail.com>
2014-05-05 10:39:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeBox::Application.config.secret_key_base = '27bef388ec8cf862c7844941025ee5f148798658f3493975b0acfb65d58c6a9ce4a58d74546fc2355a8f8f2a45c9ada0f35e747c9d5201590d0caa1c21f12461'
